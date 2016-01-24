5e26e42f668307991145df5971107a42a495ca7e
Gary Slopsema <gslopsema@gmail.com>
2016-01-24 17:13:01 -0500

    added helper Var function

--- var.go
+++ var.go
@@ -1,6 +1,10 @@
 package gogen
 
-import "go/ast"
+import (
+	"fmt"
+	"go/ast"
+	"strconv"
+)
 
 type Var struct {
 	Name string
@@ -14,4 +18,19 @@ func (me Var) Ast() ast.Expr {
 			Name: me.Name,
 		},
 	}
+}
+
+// Things that are like Var but either deserve their own name, or have
+// slightly different behaviors
+
+func String(value string) Var {
+	return Var{fmt.Sprintf(`"%s"`, value)}
+}
+
+func Int(value int) Var {
+	return Var{strconv.Itoa(value)}
+}
+
+func Pkg(value string) Var {
+	return Var{value}
 }

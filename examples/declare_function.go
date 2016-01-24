0e32c1dbcc2a6db915fcbb4aee0926051e74d4b7
Gary Slopsema <gslopsema@gmail.com>
2016-01-24 17:13:21 -0500

    Updated example

---
+++ examples/declare_function.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+	. "github.com/garslo/gogen"
+)
+
+func main() {
+	fooFuncParamName := "x"
+	fooFunc := Function{
+		Name: "foo",
+		Parameters: Types{
+			Type{
+				Name:     fooFuncParamName,
+				TypeName: StringT,
+			},
+		},
+		Body: []Statement{
+			CallFunction{
+				Func: Dotted{Pkg(Fmt), "Println"},
+				Params: []Expression{
+					Var{fooFuncParamName},
+				},
+			},
+		},
+	}
+	toPrint := Var{"y"}
+	pkg := Package{Name: "main"}
+	pkg.Import(Fmt)
+	pkg.Function(fooFunc)
+	pkg.Function(Function{
+		Name: "main",
+		Body: []Statement{
+			DeclareAndAssign{
+				toPrint,
+				String("i'm being printed from a code-genned function"),
+			},
+			CallFunction{
+				Func:   Var{fooFunc.Name},
+				Params: []Expression{toPrint},
+			},
+		},
+	})
+	pkg.WriteTo(os.Stdout)
+}
